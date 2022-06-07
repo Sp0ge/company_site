26bf81cf201afe8f546a0c08a4442e8b1057263e
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2022-06-07 23:46:44 +0300

    update

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask , render_template, url_for, request
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sql_database.db'
 db = SQLAlchemy(app)
 app.secret_key='Lk2kIC1X1RpyJSkMqAfJJltF4JkUidId4S3cpmuzxmyyxjZw6IR17Ac75tA6XNS5HDtZKRHbDaQ9zHw8V2jMSaPGfSKO2dEnif63'
-last = ""
-
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', year=datetime.now().year ,title="Главная")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', year=datetime.now().year ,title="О нас")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="80", debug=True) 
-    db.create_all()
