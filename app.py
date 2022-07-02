0bfbea954ebfeaf86ae777da96d7025aea4925db
Sp0ge <59578049+Sp0ge@users.noreply.github.com>
2022-07-03 02:39:35 +0300

    update header

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def hp():
 if __name__ == "__main__":
     db.create_all()
     global development_mode
-    development_mode = False    
+    development_mode = True    
     y = threading.Thread(target=hp)
     x = threading.Thread(target=hs)
     y.start()
